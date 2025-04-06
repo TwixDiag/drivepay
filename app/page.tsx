65b3ac6fa4b95862ad3b8118c1405a4de4b280a0
Bigsamas <art.sam2234@gmail.com>
2025-04-06 06:02:53 +0300

    Add order page url fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation';
 
 export default function Page() {
-    // const url = '/drivepay/ru';
-    const url = '/ru';
+    const url = '/drivepay/ru';
+    // const url = '/ru';
     redirect(url);
 }
